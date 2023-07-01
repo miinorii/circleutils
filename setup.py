eff49a531fa6bb8a8b0e37db91a49072afa9e5ce
miinorii <77248345+miinorii@users.noreply.github.com>
2023-07-01 12:36:53 +0200

    add numpy and pandas

--- setup.py
+++ setup.py
@@ -5,5 +5,9 @@ setup(
     author="miinorii",
     version="2023.7.1",
     packages=["circleutils"],
-    install_requires=[]
+    install_requires=[
+        "pydantic==1.10.9",
+        "numpy",
+        "pandas"
+    ]
 )
