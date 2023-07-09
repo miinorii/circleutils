022e272022766799ca608917d5a4d31ec61fa950
miinorii <77248345+miinorii@users.noreply.github.com>
2023-07-09 18:16:40 +0200

    pin

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name="circleutils",
     author="miinorii",
-    version="2023.7.1",
+    version="2023.7.9",
     packages=["circleutils"],
     install_requires=[
         "pydantic==2.0",
-        "numpy",
-        "pandas"
+        "numpy==1.24.0",
+        "pandas==2.0.3"
     ]
 )
